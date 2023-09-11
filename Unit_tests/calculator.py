d8f75131ca8a11f941c39b700fc88456a16fa74e
PeaceByPeace <ilja.123732@gmail.com>
2023-09-11 21:48:17 +0200

    Pytest check V.2

--- Unit_tests/calculator.py
+++ Unit_tests/calculator.py
@@ -1,9 +1,9 @@
 def main():
-    x = int(input("What's x? "))
+    x = input("What's x? ")
     print("x squared is", square(x))
 
 def square(n):
-    return n + n # change for + and boom
+    return n * n # change for + and boom
 
 
 if __name__ == "__main__":
